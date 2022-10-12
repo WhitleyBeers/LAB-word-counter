2423fd609a397feefa07640b417ddb0f2b21afd1
Whitley Beers <whitleybeers@gmail.com>
2022-10-12 12:56:14 -0500

    corrected function to display correct word count and error message

--- main.js
+++ main.js
@@ -6,9 +6,46 @@ const wordCounter = (value) => {
 
   if (value) {
     // COMPLETE THE LOGIC 
-    wordCount.innerHTML = `Word Count: 0`; 
+    let arr = value.split(" ");
+      console.log(arr);  
+    let count = arr.filter(word => word !== '').length;
+    wordCount.innerHTML = `Word Count: ${count}`; 
   } else {
     // if the value is empty, set the error message value to "Please input text"
-    error.innerHTML = ""; // UPDATE THIS
+    error.innerHTML = "Please input text."; // UPDATE THIS
   }
 }
+
+// OPTIONAL CHALLENGE
+const toggleMode = (btnText) => {
+  // complete the function
+}
+
+// ************************************************ //
+// **** DO NOT MODIFY THE CODE BELOW THIS LINE **** //
+// ************************************************ //
+
+// These are query selectors. We will focus on them later in the course
+const textarea = document.querySelector("textarea");
+const form = document.querySelector("form");
+const error = document.querySelector("#error");
+const wordCount = document.querySelector("#word-count");
+const toggleButton = document.querySelector("#bg-switch");
+
+// These are event listeners. We will focus on them later in the course
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // the default behavior of a form is to reload the page, we do not want that so we want to prevent that behavior
+  error.innerHTML = ""; // clear out the innerHTML on submit
+  wordCount.innerHTML = ""; // clear out the innerHTML on submit
+  const value = textarea.value; // grab the value of the text area on submit
+  wordCounter(value); // call the function and pass it the value
+});
+
+form.addEventListener("reset", () => {
+  error.innerHTML = ""; // on reset, clear the innderHTML
+  wordCount.innerHTML = ""; // on reset, clear the innderHTML
+});
+
+toggleButton.addEventListener("click", (event) => {
+  toggleMode(event.target.innerHTML);
+});
